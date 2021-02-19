5b7c3ac1eb4d5f41a7bcfd7f5953156a182c533e
stoldo <stoldo@runmyaccounts.com>
2021-02-19 15:16:11 +0100

    path update

--- src/main/java/com/stoldo/javafx_boilerplate/shared/constants/PathConstants.java
+++ src/main/java/com/stoldo/javafx_boilerplate/shared/constants/PathConstants.java
@@ -2,7 +2,7 @@ package com.stoldo.javafx_boilerplate.shared.constants;
 
 public class PathConstants {
 	
-	public static final String RESOURCE_BUNDLE_PATH = "com.stoldo.m120_rcas_projektarbeit.i18n.Resources";
+	public static final String RESOURCE_BUNDLE_PATH = "com.stoldo.javafx_boilerplate.i18n.Resources";
 	public static final String ICON_PATH = "src/main/resources/icons/";
 	public static final String IMAGE_PATH = "src/main/resources/images/";
 
